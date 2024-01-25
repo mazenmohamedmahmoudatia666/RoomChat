929caab620fcf9f7cb22eb7df601729d6fcfafe9
mazen mohamed <email>
2024-01-25 17:18:28 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -12,13 +12,15 @@ const {
   removeUser,
   getUser,
   getUsersInRoom,
+  getNumberOfUsersInRoom,
+  countAllUsers,
 } = require("./utils/users");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
@@ -35,7 +37,10 @@ io.on("connection", (socket) => {
 
     socket.join(user.room);
 
-    socket.emit("message", generateMessage("Admin", "Welcome!"));
+    socket.emit(
+      "message",
+      generateMessage("Admin", "Welcome!,\n Stand with the Gaza")
+    );
     socket.broadcast
       .to(user.room)
       .emit(
@@ -45,6 +50,8 @@ io.on("connection", (socket) => {
     io.to(user.room).emit("roomData", {
       room: user.room,
       users: getUsersInRoom(user.room),
+      numberOfUsers: getNumberOfUsersInRoom(user.room),
+      totalUsers: countAllUsers(),
     });
 
     callback();
@@ -53,7 +60,13 @@ io.on("connection", (socket) => {
   socket.on("sendMessage", (message, callback) => {
     const user = getUser(socket.id);
     const filter = new Filter();
-
+    if (
+      message.includes("كلب") ||
+      message.includes("جذمة") ||
+      message.includes("حيوان")
+    ) {
+      message = message.replace(/كلب|جذمة|حيوان/g, "***");
+    }
     if (filter.isProfane(message)) {
       return callback("Profanity is not allowed!");
     }
@@ -74,6 +87,21 @@ io.on("connection", (socket) => {
     callback();
   });
 
+  socket.on("sendPhoto", (data, callback) => {
+    const user = getUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit("photoMessage", {
+        username: user.username,
+        photo: data.photo,
+        createdAt: new Date().getTime(),
+      });
+      callback();
+    } else {
+      console.error("User not found when trying to send a photo.");
+    }
+  });
+
   socket.on("disconnect", () => {
     const user = removeUser(socket.id);
 
@@ -85,6 +113,7 @@ io.on("connection", (socket) => {
       io.to(user.room).emit("roomData", {
         room: user.room,
         users: getUsersInRoom(user.room),
+        numberOfUsers: getNumberOfUsersInRoom(user.room),
       });
     }
   });
